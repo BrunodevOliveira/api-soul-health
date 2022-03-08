a505001566d4f8859a57eb63415c0f4b5b1efd99
silas-sousa <silassousadejesus@gmail.com>
2022-03-08 14:09:09 -0300

    adicionando novos elementos ao model

--- models/User.js
+++ models/User.js
@@ -10,14 +10,17 @@ const UserSchema = new Schema({
     type: Number,
     required: true,
   },
+  
   telefone: {
     type: Number,
     required: true,
   },
+
   email: {
     type: String,
     required: true,
   },
+
   password: {
     type: String,
     required: true,
