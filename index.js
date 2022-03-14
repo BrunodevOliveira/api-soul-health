deb998912c09282990c1f4f003aad2a4f9fc89fd
silas-sousa <silassousadejesus@gmail.com>
2022-03-14 14:21:42 -0300

    configurando tsconfing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv/config");
 require("./db");
-
-
 const express = require("express");
-//const routes = require("./routes");
 
-
+import swaggerUi from "swagger-ui-express";
+//const  swaggerUi = require('swagger-ui-express')
+import swaggerDocs from "./swagger.json";
+//const swaggerDocs = require('./swagger.json')
 const app = express();
 const cors = require('cors')
 
@@ -13,9 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
 
+
 const routes = require("./routes");
 app.use('/user',routes)
 app.use('/', routes)
-app.use('/consult',routes)
+app.use('/consult', routes)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.listen(process.env.PORT || 3000);
